13e672ae98d7bf4c3ff2e132d01b7fb051312f65
henkyy <henkyyk@gmail.com>
2026-09-02 12:38:30 +0700

    Fix Compose BorderStroke color compatibility

--- android/app/src/main/java/com/henky/posqris/BorderStrokeCompat.kt
+++ android/app/src/main/java/com/henky/posqris/BorderStrokeCompat.kt
@@ -1,3 +1,8 @@
 package com.henky.posqris
 
-typealias BorderStroke = androidx.compose.foundation.BorderStroke
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+
+fun BorderStroke(width: Dp, color: Color): androidx.compose.foundation.BorderStroke =
+    androidx.compose.foundation.BorderStroke(width, Brush.solidColor(color))
