d9162e9d70f216700b15218466d796ed9dc570a2
henkyy <henkyyk@gmail.com>
2026-09-02 08:40:54 +0700

    Tambahkan kompatibilitas helper Compose untuk UI baru

---
+++ android/app/src/main/java/com/henky/posqris/ComposeCompat.kt
@@ -0,0 +1,11 @@
+package com.henky.posqris
+
+import androidx.compose.foundation.layout.RowScope as ComposeRowScope
+import androidx.compose.foundation.layout.ColumnScope as ComposeColumnScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+
+typealias RowScope = ComposeRowScope
+typealias ColumnScope = ComposeColumnScope
+
+fun Modifier.heightIn(max: Dp): Modifier = height(max)
