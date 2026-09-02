c2b2b307ee99a17153f651d8f7ccbd5dac7c7e0c
henkyy <henkyyk@gmail.com>
2026-09-02 21:36:57 +0700

    feat: add owner UI v5 activity

---
+++ android/app/src/main/java/com/henky/posqris/OwnerPosActivityV5.kt
@@ -0,0 +1,12 @@
+package com.henky.posqris
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class OwnerPosActivityV5 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { OwnerPosAppV5() }
+    }
+}
