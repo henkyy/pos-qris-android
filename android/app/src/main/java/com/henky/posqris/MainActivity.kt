c17d858cc68d8872e6eef460f724f1c05a50f4be
henkyy <henkyyk@gmail.com>
2026-09-02 22:05:33 +0700

    Aktifkan UI POS QRIS V3 sebagai aplikasi utama

--- android/app/src/main/java/com/henky/posqris/MainActivity.kt
+++ android/app/src/main/java/com/henky/posqris/MainActivity.kt
@@ -7,6 +7,6 @@ import androidx.activity.compose.setContent
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { ModernPosAppV2() }
+        setContent { ModernPosAppV3() }
     }
 }
