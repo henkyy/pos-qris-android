9fe6a9b843ad80f9f8d24bf778755009fea49db0
henkyy <henkyyk@gmail.com>
2026-09-02 14:29:41 +0700

    feat: add startup app update check

--- android/app/src/main/java/com/henky/posqris/ProfessionalPosActivity.kt
+++ android/app/src/main/java/com/henky/posqris/ProfessionalPosActivity.kt
@@ -3,10 +3,29 @@ package com.henky.posqris
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ProfessionalPosActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        lifecycleScope.launch {
+            val update = withContext(Dispatchers.IO) { AppUpdateChecker.check(this@ProfessionalPosActivity) }
+            if (update != null) {
+                AppUpdateChecker.showUpdateDialog(
+                    activity = this@ProfessionalPosActivity,
+                    update = update,
+                    onDone = { launchPos() }
+                )
+            } else {
+                launchPos()
+            }
+        }
+    }
+
+    private fun launchPos() {
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
