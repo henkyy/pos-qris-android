79616a2684b2340d4fa8c4dd08d3391b10af5120
henkyy <henkyyk@gmail.com>
2026-09-04 06:01:05 +0700

    feat(purchases): use purchase workspace

--- features/purchases/index.tsx
+++ features/purchases/index.tsx
@@ -1,3 +1,5 @@
-import EntityPage from '../shared/EntityPage'
-import { entityConfigs } from '../shared/config'
-export default function PurchasesPage() { return <EntityPage config={entityConfigs.purchases} /> }
+import PurchaseWorkspace from './PurchaseWorkspace'
+
+export default function PurchasesPage() {
+  return <PurchaseWorkspace />
+}
