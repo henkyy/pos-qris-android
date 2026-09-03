944c5014fe045cbe1c8a7c1fc11504270ba970ff
henkyy <henkyyk@gmail.com>
2026-09-03 21:55:53 +0700

    refactor: add product categories module

---
+++ features/products/categories.tsx
@@ -0,0 +1,3 @@
+import EntityPage from '../shared/EntityPage'
+import { entityConfigs } from '../shared/config'
+export default function ProductCategoriesPage() { return <EntityPage config={entityConfigs.categories} /> }
