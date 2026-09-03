717f00cdb73cac57fbd08b9181126e77131bb02a
henkyy <henkyyk@gmail.com>
2026-09-03 21:53:40 +0700

    refactor: add customers feature entry

---
+++ features/customers/index.tsx
@@ -0,0 +1,3 @@
+import EntityPage from '../shared/EntityPage'
+import { entityConfigs } from '../shared/config'
+export default function CustomersPage() { return <EntityPage config={entityConfigs.customers} /> }
