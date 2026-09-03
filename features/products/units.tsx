4c97e75069b8e3ddafb35421f995de9eb05c6048
henkyy <henkyyk@gmail.com>
2026-09-03 21:55:56 +0700

    refactor: add product units module

---
+++ features/products/units.tsx
@@ -0,0 +1,3 @@
+import EntityPage from '../shared/EntityPage'
+import { entityConfigs } from '../shared/config'
+export default function ProductUnitsPage() { return <EntityPage config={entityConfigs.units} /> }
