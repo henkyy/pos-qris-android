6ce0be11c97336fc9985fcec2e35d9474c8040ba
henkyy <henkyyk@gmail.com>
2026-09-03 21:53:59 +0700

    refactor: add payments feature entry

---
+++ features/payments/index.tsx
@@ -0,0 +1,3 @@
+import EntityPage from '../shared/EntityPage'
+import { entityConfigs } from '../shared/config'
+export default function PaymentsPage() { return <EntityPage config={entityConfigs.payments} /> }
