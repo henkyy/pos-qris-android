1a251d68ec932e133e9cecc5c4c5d63fca02fe4c
henkyy <henkyyk@gmail.com>
2026-09-03 21:53:56 +0700

    refactor: add receivables feature entry

---
+++ features/receivables/index.tsx
@@ -0,0 +1,3 @@
+import EntityPage from '../shared/EntityPage'
+import { entityConfigs } from '../shared/config'
+export default function ReceivablesPage() { return <EntityPage config={entityConfigs.receivables} /> }
