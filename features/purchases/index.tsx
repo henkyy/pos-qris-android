8a03e3b830ddb725565d68d5249c2c3d9d9a0947
henkyy <henkyyk@gmail.com>
2026-09-03 21:53:53 +0700

    refactor: add purchases feature entry

---
+++ features/purchases/index.tsx
@@ -0,0 +1,3 @@
+import EntityPage from '../shared/EntityPage'
+import { entityConfigs } from '../shared/config'
+export default function PurchasesPage() { return <EntityPage config={entityConfigs.purchases} /> }
