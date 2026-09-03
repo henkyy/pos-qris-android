201e27f4949009f8b93217deda325917bca81814
henkyy <henkyyk@gmail.com>
2026-09-04 06:58:27 +0700

    fix(auth): gate app behind Supabase login session

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import AppShell from '../components/layout/AppShell'
+import AuthGate from '../components/AuthGate'
 
 export default function HomePage() {
-  return <AppShell />
+  return <AuthGate />
 }
