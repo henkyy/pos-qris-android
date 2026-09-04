f023b67e53dd43434b3a188ffd35f0dfd9462f28
henkyy <henkyyk@gmail.com>
2026-09-04 20:20:46 +0700

    fix(products): improve category creation UX

--- features/products/categories.tsx
+++ features/products/categories.tsx
@@ -1,3 +1,3 @@
-import EntityPage from '../shared/EntityPage'
-import { entityConfigs } from '../shared/config'
-export default function ProductCategoriesPage() { return <EntityPage config={entityConfigs.categories} /> }
+'use client'
+import MasterDataWorkspace from './MasterDataWorkspace'
+export default function ProductCategoriesPage(){return <MasterDataWorkspace mode="categories"/>}
